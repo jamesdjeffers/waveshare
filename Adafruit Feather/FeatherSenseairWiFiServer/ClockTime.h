21ee57b6e4eb96257d47049b1d42452353c22980
jamesdjeffers <108284988+jamesdjeffers@users.noreply.github.com>
2022-07-25 14:13:24 -0500

    Removed internal clock operations file.

--- Adafruit Feather/FeatherSenseairWiFiServer/ClockTime.h
+++
@@ -1,9 +0,0 @@
-bool NTPEnabled = false;
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP,"pool.ntp.org", 36000, 60000);
-
-void Clock_init(){
-  timeClient.begin();
-}
